f42971f0a9ec64b7f4972f990f99f1f1fc8d9d96
matlops027 <matheus.l.alves@hotmail.com>
2022-02-20 17:16:06 -0300

    Ajustes design dashboards

--- src/app/modules/dashboard/geral/dashboard-geral.component.ts
+++ src/app/modules/dashboard/geral/dashboard-geral.component.ts
@@ -17,7 +17,7 @@ export class DashboardGeralComponent implements OnInit {
   }
 
   getObjGraphic() {
-    this.datadoughnut = { labels: ['teste', 'outro teste'], colors: ['#3F51B5'], data: [20, 10] };
+    this.datadoughnut = { labels: ['disponivel', 'investido'], colors: ['#3F51B5'], data: [20, 10] };
     this.dataHorizontalBar = { labels: [['conta 1', 'conta 2', 'conta 3', 'conta 4', 'conta 5', 'conta 6'], []], data: [[50, 40, 30, 25, 15, 45]], colors: ['#3F51B5', '#3F51B5', '#3F51B5', '#3F51B5', '#3F51B5', '#3F51B5'] };
   }
 
