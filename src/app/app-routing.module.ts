fc818e3d445e7f5198ca039f8f343b17b54af635
matlops027 <matheus.l.alves@hotmail.com>
2022-02-19 23:18:37 -0300

    Melhora da estrutura dos arquivos e roteamento dos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MenuComponent } from './components/menu/menu.component';
+import { LoginComponent } from './modules/auth/login/login.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: MenuComponent,
-    children: []
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ]
   }
 ];
 
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,15 @@
 
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from '../app-routing.module';
 import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     MenuComponent
   ],
-  imports: [],
+  imports: [
+    AppRoutingModule,
+  ],
   providers: []
 })
 export class ComponentsModule { }
